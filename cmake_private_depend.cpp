74ae0d8098cdc05432fcd9e98a700ea13155c808
aafinger <aafinger@iastate.edu>
2020-08-14 19:39:26 -0500

    updated library and cmakelists

---
+++ cmake_private_depend.cpp
@@ -0,0 +1,6 @@
+#include "cmake_private_depend.hpp"
+#include "cmake_private.hpp"
+
+int call_cmake_private_depend(){
+  return call_cmake_private()+2;
+}
---
+++ cmake_private_depend.hpp
@@ -0,0 +1,6 @@
+#ifndef CMAKE_PRIVATE_DEPEND_HPP
+#define CMAKE_PRIVATE_DEPEND_HPP
+
+int call_cmake_private_depend();
+
+#endif
